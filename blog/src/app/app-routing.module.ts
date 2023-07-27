57243b35547dbff334d901af511a69223702c7bf
sytrejo <s.y.trejo26@gmail.com>
2023-07-27 14:55:20 -0400

    adding the framework for blog articles

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  {path: 'contact' , component: ContactComponent}
+  {path: 'contact' , component: ContactComponent},
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
 ];
 
 @NgModule({
