3173985831c2d6ac35cbf9b66c04d641315eaee4
sytrejo <s.y.trejo26@gmail.com>
2023-07-27 15:19:57 -0400

    updates

---
+++ blog/src/scully.blog.config.ts
@@ -0,0 +1,10 @@
+import { ScullyConfig  } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+    projectRoot: './src',
+    projectName: 'blog',
+    outDir: './dist/static',
+    routes: {
+        
+    },
+  };
