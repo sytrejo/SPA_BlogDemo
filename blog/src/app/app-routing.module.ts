219e6056aa179fbda35c2aa43004e6e587aca9a8
sytrejo <s.y.trejo26@gmail.com>
2023-07-27 14:58:57 -0400

    all routes are working prior to adding scully

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: 'contact' , component: ContactComponent},
-  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) },
+  {path:' ' , pathMatch: 'full' , redirectTo: 'articles' },
+  {path: '**' , redirectTo: 'articles' }
 ];
 
 @NgModule({
